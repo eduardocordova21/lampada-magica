550f717a845b9cf06ba9b5daed7c1c807e8baa99
Eduardo Matheus Oliveira de Córdova <eduardocordova21@outlook.com.br>
2022-07-23 02:45:05 -0300

    resolvendo desafio

--- lampada-magica/ContentView.swift
+++ lampada-magica/ContentView.swift
@@ -8,9 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+        
+    @State var stateLightBuld: Bool = false
+    @State var potenciaLightBuld: Double = 0
+    @State var disablePotenciaSlider: Bool = true
+    @State var lightBulbYellow: Color = Color.yellow
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack(){
+            Text("Lâmpada Mágica")
+                .font(.largeTitle)
+            
+            Spacer()
+            
+            Image(systemName: "lightbulb")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200)
+            
+            Spacer()
+            
+            HStack(){
+                Text("Lâmpada: ")
+                
+                Toggle(isOn: $stateLightBuld) {
+                    if(stateLightBuld){
+                        Text("Desligar");
+                    }else{
+                        Text("Ligar")
+                    }
+                }.onChange(of: stateLightBuld) { state in
+                    potenciaLightBuld = state ? 100.0 : 0
+                    disablePotenciaSlider = !state
+                }
+            }
+            
+            HStack(){
+                Text("Potência: ")
+                
+                HStack(){
+                        Slider(
+                            value: $potenciaLightBuld,
+                            in: 0...100,
+                            step: 1
+                        )
+                        .disabled(disablePotenciaSlider)
+                        .onChange(of: potenciaLightBuld) { value in
+                            lightBulbYellow.opacity(value)
+                        }
+                    
+                        Text(String(format: "%.0f", potenciaLightBuld) + "%")
+                }
+            }
+        }
+        .padding()
+        .background(stateLightBuld ? lightBulbYellow : Color.white)
+        
     }
 }
 
