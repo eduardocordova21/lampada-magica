c4025866dc3dfb04b84caddeee49ee3650e307a7
Eduardo Matheus Oliveira de Córdova <eduardocordova21@outlook.com.br>
2022-07-23 00:53:26 -0300

    Initial Commit

---
+++ lampada-magica/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lampada-magica
+//
+//  Created by Eduardo Matheus Oliveira de Córdova on 23/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lampada-magica/lampada_magicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  lampada_magicaApp.swift
+//  lampada-magica
+//
+//  Created by Eduardo Matheus Oliveira de Córdova on 23/07/22.
+//
+
+import SwiftUI
+
+@main
+struct lampada_magicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
